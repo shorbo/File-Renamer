e1822f76b5e041656b2da06e898615006ceceac4
Shorbo Banerjea <shorbo.banerjea@gmail.com>
2026-04-25 23:03:57 -0700

    Initial Commit

---
+++ File Renamer/File_RenamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  File_RenamerApp.swift
+//  File Renamer
+//
+//  Created by Sarbajit Banerjea on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct File_RenamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
